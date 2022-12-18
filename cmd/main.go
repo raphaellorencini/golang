78a84f104d65caf4538bfb55f2c209c4483cdc8e
Raphael Lorencini <raphaellorencini@gmail.com>
2022-12-17 21:19:07 -0300

    Update order tests

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/raphaellorencini/golang/internal/order/entity"
+	"github.com/raphaellorencini/golang/internal/order/infra/database"
+)
+
+func main() {
+	order, err := entity.NewOrder("123", 10, 2)
+	if err != nil {
+		panic(err)
+	}
+	err = order.CalculateFinalPrice()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("The final price is: %f", order.FinalPrice)
+
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/orders")
+	if err != nil {
+		panic(err)
+	}
+	repository := database.NewOrderRepository(db)
+	err = repository.Save(order)
+	if err != nil {
+		panic(err)
+	}
+}
--- internal/order/entity/order_test.go
+++ internal/order/entity/order_test.go
@@ -2,8 +2,9 @@ package entity_test
 
 import (
 	"testing"
-	"github.com/stretchr/testify/assert"
+
 	"github.com/raphaellorencini/golang/internal/order/entity"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGivenAnEmptyId_WhenCreateANewOrder_ThenShouldReceiveAndError(t *testing.T) {
@@ -12,6 +13,30 @@ func TestGivenAnEmptyId_WhenCreateANewOrder_ThenShouldReceiveAndError(t *testing
 }
 
 func TestGivenAnEmptyPrice_WhenCreateANewOrder_ThenShouldReceiveAndError(t *testing.T) {
-	order := entity.Order{ID:"123"}
+	order := entity.Order{ID: "123"}
 	assert.Error(t, order.IsValid(), "invalid price")
+}
+
+func TestGivenAnEmptyTax_WhenCreateANewOrder_ThenShouldReceiveAndError(t *testing.T) {
+	order := entity.Order{ID: "123", Price: 10}
+	assert.Error(t, order.IsValid(), "invalid tax")
+}
+
+func TestGivenValidParams_WhenCallNewOrder_ThenShouldCreateOrderWithAllParams(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
+}
+
+func TestGivenValidParams_WhenCallCalculateFinalPrice_ThenShouldCalculateFinalPriceAndSetItOnFinalPriceProperty(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 2)
+	assert.NoError(t, err)
+	err = order.CalculateFinalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 12.0, order.FinalPrice)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 2.0, order.Tax)
 }
